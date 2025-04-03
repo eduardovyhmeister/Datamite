14c03561a72eddfc694eda3fc04f2fb61313e8c5
Bastien Pietropaoli <bastien.pietropaoli@insight-centre.org>
2025-04-03 15:49:46 +0100

    Model User tested with new validators.

--- ANPAHP/models/user.py
+++ ANPAHP/models/user.py
@@ -1,10 +1,42 @@
 from django.db import models
+from django.forms.models import model_to_dict
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
+NAME_MIN_LENGTH = 1
+NAME_MAX_LENGTH = 120
 
 class User(models.Model):
     """Model representing a user in our DB."""
-    first_name = models.CharField('First Name', max_length = 120)
-    last_name = models.CharField('Last Name', max_length = 120)
+    first_name = models.CharField('First Name', max_length = NAME_MAX_LENGTH, # Depends only on the DB
+                                  validators = [MaxLengthValidator(NAME_MAX_LENGTH), 
+                                                MinLengthValidator(NAME_MIN_LENGTH)])
+    last_name = models.CharField('Last Name', max_length = NAME_MAX_LENGTH,
+                                 validators = [MaxLengthValidator(NAME_MAX_LENGTH), 
+                                               MinLengthValidator(NAME_MIN_LENGTH)])
     email_address = models.EmailField('User Email', unique = True)
+
+
+    def run_validators(self):
+        """Runs all the validators for all the fields in the model."""
+        for field_name, field_value in model_to_dict(self).items():
+            model_field = getattr(User, field_name)
+            field = getattr(model_field, 'field', object())
+            validators = getattr(field, 'validators', list())
+            for validator_func in validators:
+                if field_value is not None:
+                    validator_func(field_value)
+
+
+    def save(self, *args, **kwargs):
+        """Overrides 'save()' enforcing validators whenever something is saved.
+        
+        Raises:
+            django.core.exceptions.ValidationError - If any of the validators fail.
+        """
+        self.run_validators()
+        super().save(*args, **kwargs)
+    
 
     def __str__(self): 
         """Overrides the str() methods. Used for a human readable form of the model."""
--- ANPAHP/tests/models/test_user.py
+++ ANPAHP/tests/models/test_user.py
@@ -1,12 +1,135 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-from ...models.user import User
+from ...models import user
 
 
 class UserModelTests(TestCase):
     """Class to test model 'User' in the ANPAHP app."""
     
+    
+    def test_user_creation_correct(self):
+        """A User can be created and saved into the DB."""
+        new_user = user.User(first_name = "test", 
+                        last_name = "last_test", 
+                        email_address = "test@tests.com")
+        new_user.save()
+        
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].first_name, "test")
+        self.assertEqual(users[0].last_name, "last_test")
+        self.assertEqual(users[0].email_address, "test@tests.com")
+    
+    
     def test_max_first_name_length(self):
-        """The 'first_name' field has a maximum length of 120 chars.
+        """The 'first_name' field has a maximum length of NAME_MAX_LENGTH chars.
         It should fail if we try to insert something larger."""
-        pass
+        new_user = user.User(first_name = "a"*(user.NAME_MAX_LENGTH + 1),
+                             last_name = "b" * (user.NAME_MAX_LENGTH),
+                             email_address = "test@tests.com")
+
+        with self.assertRaises(ValidationError):
+            new_user.save()
+        
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+        
+        
+    def test_min_first_name_length(self):
+        """The 'first_name' field has a minimum length of NAME_MIN_LENGTH chars.
+        It should fail if we try to insert something smaller."""
+        new_user = user.User(first_name = "",
+                             last_name = "test",
+                             email_address = "test@tests.com")
+
+        with self.assertRaises(ValidationError):
+            new_user.save()
+        
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+        
+    
+    def test_max_last_name_length(self):
+        """The 'last_name' field has a maximum length of NAME_MAX_LENGTH chars.
+        It should fail if we try to insert something larger."""
+        new_user = user.User(first_name = "a"*(user.NAME_MAX_LENGTH),
+                             last_name = "b" * (user.NAME_MAX_LENGTH + 1),
+                             email_address = "test@tests.com")
+
+        with self.assertRaises(ValidationError):
+            new_user.save()
+        
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+        
+        
+    def test_min_first_name_length(self):
+        """The 'last_name' field has a minimum length of NAME_MIN_LENGTH chars.
+        It should fail if we try to insert something smaller."""
+        new_user = user.User(first_name = "test",
+                             last_name = "",
+                             email_address = "test@tests.com")
+
+        with self.assertRaises(ValidationError):
+            new_user.save()
+        
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+    
+    
+    def test_invalid_email(self):
+        """The field 'email_address' should be a valid email address.
+        It should fail if we try to insert an invalid address."""
+        new_user = user.User(first_name = "first_test",
+                             last_name = "last_test",
+                             email_address = "test@tests")
+        with self.assertRaises(ValidationError) as e:
+            new_user.save()
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+        
+        new_user = user.User(first_name = "first_test",
+                             last_name = "last_test",
+                             email_address = "test@")
+        with self.assertRaises(ValidationError) as e:
+            new_user.save()
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+        
+        new_user = user.User(first_name = "first_test",
+                             last_name = "last_test",
+                             email_address = "test.com")
+        with self.assertRaises(ValidationError) as e:
+            new_user.save()
+        users = user.User.objects.all()
+        self.assertEqual(len(users), 0)
+        
+        
+    def test_duplicate_email(self):
+        """The field 'email_address' has to be unique.
+        It should fail when trying to add a new user with the same email address."""
+        email_address = "tests@tests.com"
+        new_user = user.User(first_name = "first_test",
+                             last_name = "last_test",
+                             email_address = email_address)
+        new_user2 = user.User(first_name = "first_duplicate",
+                              last_name = "last_duplicate",
+                              email_address = email_address)
+        new_user.save()
+        with self.assertRaises(Exception): # The type of exception doesn't really matter
+            new_user2.save()
+            
+    
+    def test_duplicate_name(self):
+        """As long as 'email_address' is unique, two users can have the same name."""
+        first_name = "first_test"
+        last_name = "last_name"
+        new_user = user.User(first_name = first_name,
+                             last_name = last_name,
+                             email_address = "test@tests.com")
+        new_user2 = user.User(first_name = first_name,
+                              last_name = last_name,
+                              email_address = "test2@tests.com")
+        new_user.save()
+        new_user2.save()
